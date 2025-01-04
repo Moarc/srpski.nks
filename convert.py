cdf3c61e6bb238cd088aae188172115e2d9bb787
Moarc <aldwulf@gmail.com>
2025-01-04 11:32:28 +0100

    Add the option to pickle nksData, mostly for development convenience.

--- convert.py
+++ convert.py
@@ -4,28 +4,45 @@ from tqdm import tqdm
 import re
 import nks
 from rsanu2 import rsanu2, sup
+import pickle
 import slob
+import argparse
 
-nksFile = open("Docum.nks", "rb")
-nksData = {}
 
-for index in [100]: #, 124
-	nksData[index] = {}
-	addr = nks.getInd(nksFile, index)
-	with tqdm(initial=nks.getInd(nksFile, index), total=nks.getInd(nksFile, index+8), unit_scale=True, unit="B") as pbar:
-		while addr < nks.getInd(nksFile, index+8):
-			try:
-				length, output = nks.getStr(nksFile, addr)
-				if output is not None:
-					nksData[index][addr] = {'content': output, 'length': length}
-					addr += length
-					pbar.update(length)
-				else:
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--pickle", action="store_true", help="save using pickle")
+parser.add_argument("-u", "--unpickle", action="store_true", help="load from pickled file")
+args = parser.parse_args()
+
+if args.unpickle:
+	with open("Docum.pkl", "rb") as f:
+		nksData = pickle.load(f)
+		f.close()
+else:
+	nksFile = open("Docum.nks", "rb")
+	nksData = {}
+
+	for index in [100]: #, 124
+		nksData[index] = {}
+		addr = nks.getInd(nksFile, index)
+		with tqdm(initial=nks.getInd(nksFile, index), total=nks.getInd(nksFile, index+8), unit_scale=True, unit="B") as pbar:
+			while addr < nks.getInd(nksFile, index+8):
+				try:
+					length, output = nks.getStr(nksFile, addr)
+					if output is not None:
+						nksData[index][addr] = {'content': output, 'length': length}
+						addr += length
+						pbar.update(length)
+					else:
+						addr += 1
+						pbar.update(1)
+				except UnicodeDecodeError:
 					addr += 1
 					pbar.update(1)
-			except UnicodeDecodeError:
-				addr += 1
-				pbar.update(1)
+if args.pickle:
+	with open("Docum.pkl", "wb") as f:
+		pickle.dump(nksData,f, protocol=pickle.HIGHEST_PROTOCOL)
+		f.close()
 
 w = slob.create("srpski.slob")
 
